ffebcff2891a30c738854ba9f5d56d72370abc14
Koose <KooseTheBlogger@gmail.com>
2022-06-17 16:38:04 +0000

    add bar in the product gridFragment

--- app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
+++ app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.shr_product_grid_fragment.view.*
 
 class ProductGridFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.shr_product_grid_fragment, container, false)
+        val view = inflater.inflate(R.layout.shr_product_grid_fragment, container, false)
+
+        // Set up the toolbar.
+        (activity as AppCompatActivity).setSupportActionBar(view.app_bar)
+        return view;
     }
 }
